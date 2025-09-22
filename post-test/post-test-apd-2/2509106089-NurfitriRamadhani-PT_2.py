f292442ec15632b10868eee3b34049808764143a
lianlian-glitch <lianlian0huan@gmail.com>
2025-09-22 23:43:52 +0800

    first commit

--- post-test/post-test-apd-2/2509106089-NurfitriRamadhani-PT_2.py
+++ post-test/post-test-apd-2/2509106089-NurfitriRamadhani-PT_2.py
@@ -1,19 +1,16 @@
-print("masukkan Nama lengkap")
+print("masukkan nama lengkap")
 nama = input("masukkan nama: ")
-
 print("masukkan NIM anda")
 nim = int(input("input NIM: "))
-
 print("masukkan harga")
 harga = int(input("masukkan harga makanan: "))
-
 pajakPecelLele = harga*0.05
 pajakMieAyam = harga*0.08
 pajakNasiPadang = harga*0.10
 totalHargaPecel = harga+pajakPecelLele
 totalHargaMie = harga+pajakMieAyam
 totalHargaNasi = harga+pajakNasiPadang
-print(f"{nama} dengan nim {nim} ingin membeli makanan dengan seharga Rp{harga}") 
+print(f"{nama} dengan nim {nim} ingin membeli makanan dengan seharga Rp{harga}")
 print(f"jika dia membeli Pecel Lele maka biaya pesanan anda RP{totalHargaPecel} setelah mendapat pajak 5%")
-print(f"jika dia membeli Mie Ayam maka biaya pesanan anda RP{totalHargaMie} setelah mendapat pajak 8%")
-print(f"jika dia membeli Nasi Padang maka biaya pesanan anda RP{totalHargaNasi} setelah mendapat pajak 10%")
+print(f"jika dia membeli Mie Ayam maka biaya pesanan anda Rp{totalHargaMie} setelah mendapat pajak 8%")
+print(f"jika dia membeli Nasi Padang maka biaya pesanan anda Rp{totalHargaNasi} setelah mendapat pajak 10%")
